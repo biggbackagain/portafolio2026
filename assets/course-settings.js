63a5b776284c84616da9bb33ae7751d1f8d70454
Bryan Garcia <garciagg2193@gmail.com>
2026-09-17 01:59:44 -0600

    chore: restaurar api_url de produccion

--- assets/course-settings.js
+++ assets/course-settings.js
@@ -1,2 +1,2 @@
 // Public URL only, never credentials. Empty uses /api on the same origin.
-window.COURSE_API_BASE = "https://7417c6f137bd44.lhr.life";
+window.COURSE_API_BASE = "https://api.irangarcia.dev";
